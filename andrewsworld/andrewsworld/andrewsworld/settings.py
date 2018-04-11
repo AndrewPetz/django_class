c76c82d514f6ca3e5c269618af722208d6dc5763
Andrew Petz <nannooskeeska@gmail.com>
2018-04-10 19:10:03 -0500

    Finished blog

--- andrewsworld/andrewsworld/andrewsworld/settings.py
+++ andrewsworld/andrewsworld/andrewsworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '60$qq*6uwym^-lz6qn*8zy7#$i-rz-mc*xu&%ivcsc017(&xn%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['144.202.51.154', 'yoururl.com']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'sitepages',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/pics/'
 MEDIA_ROOT = BASE_DIR
--- andrewsworld/andrewsworld/andrewsworld/urls.py
+++ andrewsworld/andrewsworld/andrewsworld/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import posts.views
+import sitepages.views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', posts.views.home, name="home"),
+    path('about/', sitepages.views.about, name="about"),
     path('posts/<int:post_id>/', posts.views.post_details, name="post_detail"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
